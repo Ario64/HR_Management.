e48276708887a67ae1f4d698cc641be6915f2c4d
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-09-03 19:48:24 +0330

    implement uint of work

---
+++ HR_Management.Application/IUintOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using HR_Management.Domain.Repositories;
+
+namespace HR_Management.Application.UintOfWork;
+
+public interface IUnitOfWork
+{
+    IGenericRepository<T> GenericRepository<T>() where T : class;
+    Task SaveChangesAsync(CancellationToken cancellationToken);
+    void SaveChanges();
+}
