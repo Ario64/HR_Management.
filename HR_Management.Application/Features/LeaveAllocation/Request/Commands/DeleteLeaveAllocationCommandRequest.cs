3eda245f6fde801bdde60b0534d645b13dd17aff
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-09-27 16:23:01 +0330

    implement delete leave allocation using CQRS

---
+++ HR_Management.Application/Features/LeaveAllocation/Request/Commands/DeleteLeaveAllocationCommandRequest.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace HR_Management.Application.Features.LeaveAllocation.Request.Commands;
+
+public record DeleteLeaveAllocationCommandRequest(int id) : IRequest<bool>
+{
+}
---
+++ HR_Management.Application/Features/LeaveAllocation/Request/Commands/EditLeaveAllocationCommandRequest.cs
@@ -0,0 +1,8 @@
+﻿using HR_Management.Domain.DTOs.LeaveAllocationDTOs;
+using MediatR;
+
+namespace HR_Management.Application.Features.LeaveAllocation.Request.Commands;
+
+public record EditLeaveAllocationCommandRequest(int id, EditLeaveAllocationDto EditLeaveAllocationDto) : IRequest<bool>
+{
+}
