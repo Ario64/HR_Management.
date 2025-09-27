2ff827708b5b2b4cc683b3e8dd8d140bbf924b7b
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-09-27 15:58:01 +0330

    implement get leave allocation and get leave allocation list request and handler by CQRS

---
+++ HR_Management.Application/Features/LeaveAllocation/Handler/Queries/GetLeaveAllocationListRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HR_Management.Application.Features.LeaveAllocation.Request.Queries;
+using HR_Management.Application.UintOfWork;
+using HR_Management.Domain.DTOs.LeaveAllocationDTOs;
+using MediatR;
+
+namespace HR_Management.Application.Features.LeaveAllocation.Handler.Queries;
+
+public class GetLeaveAllocationListRequestHandler : IRequestHandler<GetLeaveAllocationListRequest, IReadOnlyList<LeaveAllocationDto>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+    public GetLeaveAllocationListRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<IReadOnlyList<LeaveAllocationDto>> Handle(GetLeaveAllocationListRequest request, CancellationToken cancellationToken)
+    {
+        var leaveAllocationList = await _unitOfWork.GenericRepository<HR_Management.Domain.Entities.LeaveAllocation>()
+                                                   .GetAllAsync(cancellationToken);
+
+        var mappedLeaveAllocationList = _mapper.Map<IReadOnlyList<LeaveAllocationDto>>(leaveAllocationList);
+
+        return mappedLeaveAllocationList;
+    }
+}
