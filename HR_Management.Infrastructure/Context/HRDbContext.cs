2ca40527deffdd37e1cd3775e546aabb70da265b
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-09-29 18:46:43 +0330

    override save changes and save changes async to handle audit properties

--- HR_Management.Infrastructure/Context/HRDbContext.cs
+++ HR_Management.Infrastructure/Context/HRDbContext.cs
@@ -1,4 +1,5 @@
-﻿using HR_Management.Domain.Entities;
+﻿using HR_Management.Domain.Common;
+using HR_Management.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace HR_Management.Infrastructure.Context;
@@ -22,6 +23,44 @@ public class HRDbContext : DbContext
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(HRDbContext).Assembly);
 
+    }
+
+    #endregion
+
+    #region Save change async configuration
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+        {
+            entry.Entity.LastModifiedDate = DateTime.UtcNow;
+
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreateDate = DateTime.UtcNow;
+            }
+        }
+
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    #endregion
+
+    #region Save change configuration
+
+    public override int SaveChanges()
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+        {
+            entry.Entity.LastModifiedDate = DateTime.UtcNow;
+
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreateDate = DateTime.UtcNow;
+            }
+        }
+
+        return base.SaveChanges();
     }
 
     #endregion
