71d1b9a4be8c35ebb9cb51e155be2b5fdd9d0792
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-10-01 16:50:05 +0330

    add MailKit for sending email

--- HR_Management.Application/Models/EmailSetting.cs
+++ HR_Management.Application/Models/EmailSetting.cs
@@ -2,7 +2,9 @@
 
 public class EmailSetting
 {
-    public required string  ApiKey { get; set; }
-    public required string SenderAddress { get; set; }
-    public required string SenderName { get; set; }
+    public string? SenderAddress { get; set; }
+    public string? SenderPassword { get; set; }
+    public string? SenderName { get; set; }
+    public string? MailServer { get; set; }
+    public int MailPort { get; set; }
 }
